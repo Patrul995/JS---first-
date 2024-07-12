d31b768167cde94c0b54ee7bb47197504c57838a
Patrul <aliwka9532@gmail.com>
2024-07-12 19:46:23 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+////// 1st task //////
+// let num = prompt ("Write your name");
+// alert ("Hello, " + num);
+
+
+////// 2nd task //////
+// let num1 = prompt ("Write your birth. year");
+// const num2 = 2024;
+// alert (num2 - num1);
+
+
+////// 3rd task //////
+// let num1 = prompt ("Write number");
+// alert (num1*4);
+
+
+////// 4th task //////
+// let num1 = prompt ("Write number");
+// alert (num1*num1*3.14);
+
+
+////// 5th task //////
+// let num1 = prompt ("Километры");
+// let num2 = prompt ("часы");
+// let num3 = "км/ч";
+// alert (num1/num2 + num3);
+
+
+////// 6th task //////
+// let num1 = prompt ("USD to EURO");
+// let dollar =' EURO';
+// alert (num1*0.92 + dollar);
+
+
+////// 7th task //////
+// let num1 = prompt ("Write GB");
+// let num2 = 0.82;
+// let num3 = Math.floor(num1 / num2);
+// alert (num3);
+
+
+////// 8th task //////
+// let num1 = prompt ("Money");
+// let num2 = prompt ("Chocolates");
+// let num3 = Math.floor(num1 / num2);
+// let num4 = Math.floor(num1 % num2);
+// alert (num3);
+// alert (num4);
+
+
+////// 9th task //////
+// let num1 = prompt("Write your 1st number");
+// let num2 = prompt("Write your 2nd number");
+// let num3 = prompt("Write your 3nd number");
+// alert(num3 + num2 + num1);
+
+
+// nmb = prompt('Введите трехзначное число: ');
+// var a = Number(nmb % 10);
+// var b = Number((nmb - a) / 10 % 10);
+// var c = Number((nmb - a - b * 10) / 100);
+// alert('Перевертыш: ' + (a * 100 + b * 10 + c));
+
+
+////// 10th task //////
+// const i = prompt("number");
+// if (i % 2)
+//     { alert('odd'); }
+// else
+//     { alert('even'); };
